afe631d455d9e2bd57957203d49d2b60baef11ac
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-13 17:06:57 -0500

    input box added for name creation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log("Script linked");
+const game = {
+
+  time: 0,
+  experiment: 0,
+  tamagotchi: [],
+
+  loadScreen(){
+    $askForName = $('<div/>').attr('id', 'askForName');
+    $textBox = $('<input/>').attr('size', '20');
+    $askForName.append($textBox);
+    $('main').append($askForName);
+
+
+  },
+
+  itsAlive(){
+
+  },
+
+
+
+  setTimer(){
+    const timer = setInterval(() => {
+      this.time++;
+      $('#age').text(`Organism Age: ${this.age}`);
+      $('#timer').text(`Experiment Time: ${this.time}s`);
+
+    }, 1000);
+  }
+	
+
+}
+
+game.loadScreen();
---
+++ js/tamagotchi.js
@@ -0,0 +1,10 @@
+class Tamagotchi {
+	constructor(name){
+		this.name = name;
+		this.age = 0;
+		this.hunger = 0;
+		this.boredom = 0;
+		this.sleepiness = 0;
+	},
+	
+}
