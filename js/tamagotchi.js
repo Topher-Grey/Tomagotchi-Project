d6bdc5ae8c318745fb9637469e8035a2a77b7647
Christopher Lizon <Christopher.Lizon@gmail.com>
2019-07-13 22:05:46 -0500

    Timer and class elements updated

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -1,10 +1,19 @@
 class Tamagotchi {
-	constructor(name){
+	constructor(name, age = 1, hunger = 0, boredom = 0, sleepiness = 0){
 		this.name = name;
-		this.age = 0;
+		this.age = 1;
 		this.hunger = 0;
 		this.boredom = 0;
 		this.sleepiness = 0;
+	}
+	feed(){
+
+	}
+	play(){
+
+	}
+	rest(){
+
 	}
 	
 }
